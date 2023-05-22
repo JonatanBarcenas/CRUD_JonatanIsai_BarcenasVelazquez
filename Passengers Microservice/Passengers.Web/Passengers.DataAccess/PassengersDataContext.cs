d6e18682261d6ec2085fba922060c3bcd6ddf417
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-05-22 09:10:55 -0600

    First Microservice about passegners

---
+++ Passengers Microservice/Passengers.Web/Passengers.DataAccess/PassengersDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Passengers.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passengers.DataAccess
+{
+    public class PassengersDataContext : DbContext
+    {
+        public virtual DbSet<Passenger> Passenger { get; set; }
+    }
+}
